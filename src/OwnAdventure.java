6e3af103d48c24fd9b256736ced68fb1a67b3feb
U have'nt <whaaqaaaidchbfgvruihgiudfgiufhjifo@gmail.com>
2018-12-12 19:59:34 -0800

    Update OwnAdventure.java

--- src/OwnAdventure.java
+++ src/OwnAdventure.java
@@ -4,54 +4,31 @@ public class OwnAdventure {
 
 	public static void main(String[] args) {
 
-		JOptionPane.showMessageDialog(null,
-				"One day in Africa, Lily was sitting in canoe floating and rowing down the Amazon River.");
-
-		JOptionPane.showMessageDialog(null, "Suddenly, a large crocodile emerged. ");
-
-		String crocodile = JOptionPane.showInputDialog("Should Lily get eaten or swim to shore?");
-		if (crocodile.equals("Swim to shore")) {
-			JOptionPane.showMessageDialog(null, "Lily swam to shore, and collapsed on the bank");
-
-		} else {
-			JOptionPane.showMessageDialog(null, "Lily, got eaten!");
-			JOptionPane.showMessageDialog(null, "Game Over!");
-			System.exit(1);
-		}
-
-		JOptionPane.showMessageDialog(null,
-				"After Lily collapsed on the bank, she stood up and wandered cautiously into the forest.");
-
-		JOptionPane.showMessageDialog(null, "As Lily walked, she became more and more aware of her surroundings.");
-
-		JOptionPane.showMessageDialog(null, "The leaves on the trees rustled in the unnatural silence.");
-
-		JOptionPane.showMessageDialog(null,
-				"In the blink of an eye, as Lily rounded the corner, there stood a ancient temple covered in vines.");
-
-		String tiger = JOptionPane
-				.showInputDialog("Should Lily enter the temple or run away from the incoming angry tiger?");
-
-		if (tiger.equals("Enter")) {
-			JOptionPane.showMessageDialog(null, "Lily ran away from the slobbering tiger into the temple");
-		} else {
-			JOptionPane.showMessageDialog(null, "Lily got torn apart by the ravenous tiger!");
-			System.exit(1);
-		}
-		
-		JOptionPane.showMessageDialog(null, "Inside the temple, there were torches that suddenly lit as Lily walked passed.");		
-		
-		JOptionPane.showMessageDialog(null, " Suddenly an errie noise swept through the stone halls and Lily hugged herself in fear.");
-		
-		JOptionPane.showMessageDialog(null, "Then that errie noise transformed into a growl and the ravenous tiger reappeared.");
-		
-		JOptionPane.showMessageDialog(null, " THe tiger lunged at Lily and still to this day, the temple lies a monument to the death of Lily.");
-		
+	JOptionPane.showMessageDialog(null, "One day, a girl went canoeing on the Amazon River. ");	
+	JOptionPane.showMessageDialog(null, "Suddenly, a crocodile emerged and started to swim sneakily to the canoe.");
+	String crocodile = JOptionPane.showInputDialog("Should she jump from the canow and swim to shore or take her chances in the boat?");
+	
+	if (crocodile.equals("swim to shore")) {
+		JOptionPane.showMessageDialog(null, "The girl jumped from the canoe and swam to shore.");
 		
 	}
+	else {JOptionPane.showMessageDialog(null, "She stayed in the boat and the crocodile creeped closer and ramed into the side, the girl fell into the water and didn't resurface.");
+	System.exit(0);}
+	JOptionPane.showMessageDialog(null, "She lay on the bank out of breath, but stood up and walked deep into the jungle.");
 	
-	
-	
-	
+	JOptionPane.showMessageDialog(null, "As the girl walked deeper into the jungle, the trees grew eerily taller and vines covered the floor more heavily.");
+	JOptionPane.showMessageDialog(null, "A ravenous tiger appeared from out behind the tree, its head perked as the girl tried to move away silently.");
+	 String tiger = JOptionPane.showInputDialog("Should the girl run or stay still and hope it goes away?");
+	 
+	 if (tiger.equals("run")) {
+	JOptionPane.showMessageDialog(null, "She ran through the jungle as the tiger looked away for the slightest second, and escaped the tiger's plausible wrath.");
+	 }
+	 else {
+		 JOptionPane.showMessageDialog(null, "The tiger saw the girl as vulnerable prey and chased her just as she tried to run. Eventually the tiger caught up to her and that was the girl's most unfortunate accident.");
+			System.exit(0);
+	 }
+	 }
+	}
 
-}
+	
+	
